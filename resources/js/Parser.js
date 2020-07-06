05423e1eeb57c51ceaaf36c34b87b93400a390dc
swag31415 <kidswagatam18@gmail.com>
2020-07-06 19:24:42 -0400

    added folder traversal

--- resources/js/Parser.js
+++ resources/js/Parser.js
@@ -7,3 +7,15 @@ function getSizes(folder) {
     return folder.folders.map(f => f.size) // All the folder sizes
             .concat(folder.files.map(f => f.size)) // AND all the file sizes
 }
+
+function gen_data(folder) {
+    var names = getNames(folder)
+    return {
+        labels: names,
+        datasets: [{
+          label: folder.name,
+          backgroundColor: pattern.generate(gen_colors(names.length)),
+          data: getSizes(folder)
+        }]
+    }
+}
