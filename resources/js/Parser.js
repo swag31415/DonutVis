3e961884283d0087749aea129b9dc8959a8010b6
swag31415 <kidswagatam18@gmail.com>
2020-07-06 21:43:53 -0400

    Added option to view sizes by filetype

--- resources/js/Parser.js
+++ resources/js/Parser.js
@@ -8,14 +8,46 @@ function getSizes(folder) {
             .concat(folder.files.map(f => f.size)) // AND all the file sizes
 }
 
-function gen_data(folder) {
+function get_types(folder) {
+    var result = new Set()
+    folder.files.forEach(f => result.add(f.ext))
+    folder.folders.forEach(f => {
+        get_types(f).forEach(g => result.add(g))
+    })
+    return [...result]
+}
+
+function get_size_by_extension(folder, extension) {
+    size = 0
+    folder.files.filter(f => f.ext == extension)
+                .forEach(f => size += f.size)
+    folder.folders.map(f => get_size_by_extension(f, extension))
+                .forEach(s => size += s)
+    return size
+}
+
+function get_sizes_by_type(folder) {
+    var types = get_types(folder)
+    var sizes = []
+    for (let i = 0; i < types.length; i++) {
+        sizes[i] = get_size_by_extension(folder, types[i]);
+    }
+    return sizes
+}
+
+function gen_data(folder, by_ext = false) {
     var names = getNames(folder)
+    var sizes = getSizes(folder)
+    if (by_ext) {
+        names = get_types(folder)
+        sizes = get_sizes_by_type(folder)
+    }
     return {
         labels: names,
         datasets: [{
           label: folder.name,
           backgroundColor: pattern.generate(gen_colors(names.length)),
-          data: getSizes(folder)
+          data: sizes
         }]
     }
 }
